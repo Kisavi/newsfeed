fb3bd2d582cff9d43385a22e0cc80a5f4c1102f5
Kisavi <deniskisavi@gmail.com>
2022-07-21 06:25:07 +0800

    create an alert when a user makes a comment

---
+++ newsfeed/src/app/components/comment-alert/comment-alert.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CommentAlertComponent } from './comment-alert.component';
+
+describe('CommentAlertComponent', () => {
+  let component: CommentAlertComponent;
+  let fixture: ComponentFixture<CommentAlertComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CommentAlertComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CommentAlertComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ newsfeed/src/app/components/comment-alert/comment-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-alert',
+  templateUrl: './comment-alert.component.html',
+  styleUrls: ['./comment-alert.component.css']
+})
+export class CommentAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
