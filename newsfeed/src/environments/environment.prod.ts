3f3733db28ff093405c8d6110e41a891068f73dd
Kisavi <deniskisavi@gmail.com>
2022-07-26 02:11:36 +0800

    fix - create a function to toggle an individual comment section

--- newsfeed/src/environments/environment.prod.ts
+++ newsfeed/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl:"https://jsonfeedsapi.herokuapp.com"
+  baseUrl:"http://localhost:3000"
 };
--- newsfeed/src/environments/environment.ts
+++ newsfeed/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl:"https://jsonfeedsapi.herokuapp.com"
+  baseUrl:"http://localhost:3000"
 };
 
 /*
