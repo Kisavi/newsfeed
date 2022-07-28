1d924359213d50308c77fbae0c9f199727f1c149
Kisavi <deniskisavi@gmail.com>
2022-07-28 18:11:53 +0800

    Auto-generated commit [ci skip]

---
+++ scripts.8f042435e85a4a18.js
@@ -0,0 +1,1 @@
+function openNav(){document.getElementById("myNavbar").style.width="250px",document.body.classList.toggle("lock-scroll")}function closeNav(){document.getElementById("myNavbar").style.width="0"}
